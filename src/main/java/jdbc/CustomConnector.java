70902aeba1cfa9d1bf05a4fa2622a27a30d555ce
Dmitry Golub <dmgolub@mail.ru>
2023-06-17 22:45:52 +0400

    CustomConnector fixed.

--- src/main/java/jdbc/CustomConnector.java
+++ src/main/java/jdbc/CustomConnector.java
@@ -7,14 +7,10 @@ import java.sql.SQLException;
 public class CustomConnector {
 
     public Connection getConnection(String url) throws SQLException {
-        try (Connection connection = DriverManager.getConnection(url)) {
-            return connection;
-        }
+        return DriverManager.getConnection(url);
     }
 
     public Connection getConnection(String url, String user, String password) throws SQLException {
-        try (Connection connection = DriverManager.getConnection(url, user, password)) {
-            return connection;
-        }
+        return DriverManager.getConnection(url, user, password);
     }
 }
